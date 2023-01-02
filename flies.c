126c3bc1387e7e01a1e5f0c3bc32b0d054283d12
dmfedorin <m7996@students.eischools.ae>
2023-01-02 13:57:54 +0400

    progress on flies

--- flies.c
+++ flies.c
@@ -10,23 +10,32 @@
 #define TITLE "flies: genetic algorithm"
 #define WIDTH 600
 #define HEIGHT 600
+#define TPS 60
 
 /* configuration for drawing. */
-#define DRAW_FLY_COLOR build_color(0, 0, 0)
-#define DRAW_BG_COLOR build_color(190, 190, 190)
-#define DRAW_OBSTACLE_COLOR build_color(90, 0, 0)
-#define DRAW_GOAL_COLOR build_color(0, 200, 0)
+#define DRAW_FLY_R 0
+#define DRAW_FLY_G 0
+#define DRAW_FLY_B 0
+#define DRAW_BG_R 190
+#define DRAW_BG_G 190
+#define DRAW_BG_B 190
+#define DRAW_OBSTACLE_R 90
+#define DRAW_OBSTACLE_G 0
+#define DRAW_OBSTACLE_B 0
+#define DRAW_GOAL_R 0
+#define DRAW_GOAL_G 200
+#define DRAW_GOAL_B 0
 #define DRAW_FLY_SIZE 8
 #define DRAW_GOAL_SIZE GA_GOAL_SIZE
 
 /* configuration for genetic algorithm. */
 #define GA_FLY_MAX_MOVES 90
 #define GA_FLY_SPEED 7
-#define GA_FLY_COUNT 10
 #define GA_FLY_START_X (WIDTH / 2)
 #define GA_FLY_START_Y (HEIGHT * 3 / 4)
 #define GA_GOAL_SIZE 8
 #define GA_MUTATION_RATE 5
+#define GA_GENERATION_SIZE 500
 
 struct goal {
     struct goal const *next;
@@ -45,14 +54,15 @@ struct fly {
     bool dead;
 };
 
-static SDL_Color build_color(uint8_t r, uint8_t g, uint8_t b);
 static struct fly fly_create(struct fly const *parent);
 static double fly_fitness(struct fly const *fly);
 static void fly_move(struct fly *fly);
 static int random_int(int lower_bound, int upper_bound);
 static int clamp(int lower_bound, int n, int upper_bound);
+static void reset_generation(void);
 
-static struct fly flies[GA_FLY_COUNT];
+static struct fly flies[GA_GENERATION_SIZE];
+static int generation_num = 1;
 
 int main(void)
 {
@@ -72,28 +82,49 @@ int main(void)
         return -1;
     }
 
+    reset_generation();
+
     while (running) {
         SDL_Event e;
+        size_t i;
+        bool generation_dead = true;
 
         while (SDL_PollEvent(&e) == 1) {
             if (e.type == SDL_QUIT)
                 running = false;
         }
+
+        /* game logic. */
+        for (i = 0; i < GA_GENERATION_SIZE; ++i) {
+            fly_move(&flies[i]);
+            generation_dead = generation_dead && flies[i].dead;
+        }
+
+        if (generation_dead) {
+            ++generation_num;
+            reset_generation();
+        }
+
+        /* rendering. */
+        SDL_SetRenderDrawColor(rend, DRAW_BG_R, DRAW_BG_G, DRAW_BG_B, 255);
+        SDL_RenderClear(rend);
+
+        SDL_SetRenderDrawColor(rend, DRAW_FLY_R, DRAW_FLY_G, DRAW_FLY_B, 255);
+        for (i = 0; i < GA_GENERATION_SIZE; ++i) {
+            SDL_Rect r;
+
+            r.x = flies[i].x - DRAW_FLY_SIZE / 2;
+            r.y = flies[i].y - DRAW_FLY_SIZE / 2;
+            r.w = r.h = DRAW_FLY_SIZE;
+            SDL_RenderFillRect(rend, &r);
+        }
+
+        SDL_RenderPresent(rend);
+        SDL_Delay(1000 / TPS);
     }
 
     SDL_Quit();
     return 0;
-}
-
-static SDL_Color build_color(uint8_t r, uint8_t g, uint8_t b)
-{
-    SDL_Color col;
-
-    col.r = r;
-    col.g = g;
-    col.b = b;
-    col.a = 255;
-    return col;
 }
 
 static struct fly fly_create(struct fly const *parent)
@@ -117,6 +148,7 @@ static struct fly fly_create(struct fly const *parent)
 
 static double fly_fitness(struct fly const *fly)
 {
+    return 0.0;
 }
 
 static void fly_move(struct fly *fly)
@@ -135,8 +167,8 @@ static void fly_move(struct fly *fly)
     if (next_move > fly->moves_set)
         ++fly->moves_set;
 
-    fly->x = clamp(0, fly->x + moves_x[next_move], WIDTH);
-    fly->y = clamp(0, fly->y + moves_y[next_move], HEIGHT);
+    fly->x = clamp(0, fly->x + fly->moves_x[next_move], WIDTH);
+    fly->y = clamp(0, fly->y + fly->moves_y[next_move], HEIGHT);
     --fly->moves_left;
 }
 
@@ -158,4 +190,34 @@ static int clamp(int lower_bound, int n, int upper_bound)
     }
 
     return n < upper_bound ? n > lower_bound ? n : lower_bound : upper_bound;
+}
+
+static void reset_generation(void)
+{
+    size_t i;
+    double best_fitness = fly_fitness(&flies[0]);
+    struct fly const *fittest_fly = &flies[0];
+
+    /*
+     * generation 1 is the first generation, so there is no point in resetting
+     * to the fittest fly, since they are all the same.
+     */
+    if (generation_num == 1) {
+        for (i = 0; i < GA_GENERATION_SIZE; ++i)
+            flies[i] = fly_create(NULL);
+
+        return;
+    }
+
+    for (i = 1; i < GA_GENERATION_SIZE; ++i) {
+        double fitness = fly_fitness(&flies[i]);
+
+        if (fitness > best_fitness) {
+            fittest_fly = &flies[i];
+            best_fitness = fitness;
+        }
+    }
+
+    for (i = 0; i < GA_GENERATION_SIZE; ++i)
+        flies[i] = fly_create(fittest_fly);
 }
